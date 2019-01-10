5888511acc0de71fde160eb99c087667a7ddef15
Anqi-Dai <adai@bu.edu>
2019-01-10 17:33:37 -0500

    The limma DE

--- 04_BewleyData_DE_limma.R
+++ 04_BewleyData_DE_limma.R
@@ -2,32 +2,35 @@
 
 library(tidyverse)
 library(limma)
+library(Biobase)
 
 # load the data (filtered)
 fdat <- read_rds('processedData/bewley_data_healthy_subjects_rma_FILTERED.RDS')
+pData(fdat)$Infect <- as.character(pData(fdat)$Infect)
 
-## wrapper for limma, assumes data is already log2 normalized
-run_limma <- function(eset, class_id, control, treatment)
-{
-  control_inds <- which(pData(eset)[, class_id] == control)
-  treatment_inds <- which(pData(eset)[, class_id] == treatment)
-  
-  eset.control <- eset[, control_inds]
-  eset.treatment <- eset[, treatment_inds]
-  eset.compare <- eset[, c(control_inds, treatment_inds)]
-  condition <- as.character(pData(eset.compare)[, class_id])
-  colData <- data.frame(condition=as.character(pData(eset.compare)[, class_id]))
-  
-  design <- model.matrix(~ 0 + factor(condition))
-  colnames(design) <- levels( factor(condition))
-  fit <- lmFit(eset.compare, design)
-  command_str <- paste("makeContrasts(",
-                       "(", treatment , "-", control, ")", 
-                       ",levels = design)", sep = "")
-  contrast.matrix <- eval(parse(text =command_str)) 
-  fit2 <- contrasts.fit(fit, contrast.matrix)
-  fit2 <- eBayes(fit2)
-  res <- topTable(fit2, coef=1, adjust="BH", number =length(fit2) ,
-                  sort.by = "none")
-  return(res)
-}
+
+# Limma
+infect_status <- pData(fdat)$Infect
+design <- model.matrix(~ 0 + infect_status)
+colnames(design) <- c('CGSP14','none')
+fit <- lmFit(fdat, design)
+
+command_str <- paste("makeContrasts(","(", 'CGSP14' , "-", 'none', ")", ",levels = design)", sep = "")
+contrast.matrix <- eval(parse(text =command_str)) 
+
+fit2 <- contrasts.fit(fit, contrast.matrix)
+fit2 <- eBayes(fit2)
+
+# organize the limma result
+limma.res <- data.frame(
+  t_stats = fit2$t %>% as.data.frame,
+  pval = fit2$p.value %>% as.data.frame
+) %>%
+  rename(t_stats = names(.)[1],
+         pval= names(.)[2]) %>%
+  rownames_to_column %>%
+  rename(probesetID = rowname) %>%
+  mutate(padj = p.adjust(pval, method = 'BH')) %>%
+  filter(padj < 0.05) %>%
+  arrange(padj)
+
